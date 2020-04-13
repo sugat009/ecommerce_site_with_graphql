b39b3e6c14de5b12e9543d5f88f2791e0011b29a
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-04-13 18:02:44 +0545

    graphql used to query CollectionPreview component and Collection Component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {ApolloProvider} from "react-apollo";
+import {createHttpLink} from "apollo-link-http";
+import {InMemoryCache} from "apollo-cache-inmemory";
+import {ApolloClient, gql} from "apollo-boost";
 
-import { store, persistor } from './redux/store';
+import {persistor, store} from './redux/store';
 
 import './index.css';
 import App from './App';
 
+
+// To create a connection to Backend
+const httpLink = createHttpLink({
+    uri: "https://crwn-clothing.com"
+});
+
+// For caching
+const cache = new InMemoryCache();
+
+// For client
+const client = new ApolloClient({
+    link: httpLink,
+    cache
+});
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+        <Provider store={store}>
+            <BrowserRouter>
+                <PersistGate persistor={persistor}>
+                    <App/>
+                </PersistGate>
+            </BrowserRouter>
+        </Provider>
+    </ApolloProvider>,
+    document.getElementById('root')
 );
---
+++ src/pages/collection/collection.container.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Query} from "react-apollo";
+import {gql} from "apollo-boost";
+
+import Spinner from "../../components/spinner/spinner.component";
+import CollectionPage from "./collection.component";
+
+const GET_COLLECTION_BY_TITLE = gql`
+    query getCollectionsByTitle($title: String!) {
+        getCollectionsByTitle(title: $title) {
+            id
+            title
+            items {
+                id
+                name
+                price
+                imageUrl
+            }
+        }
+    }
+`;
+
+const CollectionContainer = ({match}) => {
+    return (
+        <Query
+            query={GET_COLLECTION_BY_TITLE}
+            variables={{title: match.params.collectionId}}
+        >
+            {
+                ({loading, data}) => {
+                    if (loading) return <Spinner/>;
+                    const {getCollectionsByTitle} = data;
+                    return <CollectionPage collection={getCollectionsByTitle}/>;
+                }
+            }
+        </Query>
+    );
+};
+
+export default CollectionContainer;
